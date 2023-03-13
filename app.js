573a04452124a8ee92a9571543c2e0d273764006
balasubrahmanyam <baluvv256@gmail.com>
2023-03-13 01:45:11 +0000

    api_routin_solution

---
+++ app.js
@@ -0,0 +1,14 @@
+let express = require("express");
+let app = express();
+
+app.get("/", (request, response) => {
+  response.send("Home Page");
+});
+
+app.get("/about", (request, response) => {
+  response.send("About Page");
+});
+
+module.exports = app;
+
+app.listen(3000);
